83d96105ebea31b446e1614c2d3315bf91f8d4c6
tavito47 <ivancitoxciempre@gmail.com>
2023-07-25 17:43:32 -0400

    test-1 iniciando HeaderListo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
-import Header from './Header';
+import Header from './components/Header';
 import './app.css';
+import Footer from './components/Footer';
+import './fonts/fonts.css'
 
 function App() {
   return (
     <>
+    
+    <p class="regular">Este texto usa MiFuenteRegular.</p>
+    <p class="bold">Este texto usa MiFuenteRegular.</p>
+    <p class="light">Este texto usa MiFuenteRegular.</p>
+    <p class="medium">Este texto usa MiFuenteRegular.</p>
     <Header/>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <h1>asddddddd</h1>
+    </>
+
+  )
+}
+
+export default Footer
